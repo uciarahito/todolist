632696bab58e8b3c1fe4486e1d7ef7bc143be115
arahito <uarahito@yahoo.com>
2017-05-05 19:50:20 +0700

    install package and create folder skeleton

---
+++ server/routes/index.js
@@ -0,0 +1,8 @@
+'use strict';
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+    res.send('Haiiiiiii')
+})
+
+module.exports = router
---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const passport = require('passport')
+const Strategy = require('passport-local').Strategy
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+var User = require('./models/user')
+var controller = require('./controllers/userController')
+const jwt = require('jsonwebtoken')
+const app = express()
+var cors = require('cors')
+
+mongoose.connect('mongodb://localhost/todo-list');
+
+// NOTE: set
+app.set('port', process.env.PORT || 3000)
+
+// NOTE: use
+/* jika gagal melalui instal cors, maka tambah code di bawah ini */
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*")
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token")
+//     next()
+// })
+app.use(require('morgan')('dev'))
+app.use(require('body-parser').urlencoded({
+    extended: false
+}));
+app.use(require('body-parser').json());
+app.use(cors())
+
+// passport.use(new Strategy(controller.signin));
+
+app.use(passport.initialize());
+
+app.use('/', require('./routes'))
+
+// NOTE: run
+app.listen(app.get('port'), () => {
+    console.log('Listening on port ' + app.get('port'));
+})
